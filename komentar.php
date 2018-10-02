17d13e1758bc75fcbae50157b66d24053f94c80c
Muhammad Luqman <43492565+mLuckmen@users.noreply.github.com>
2018-10-02 16:59:43 +0700

    Add files via upload

--- komentar.php
+++ komentar.php
@@ -1,3 +1,20 @@
-<?php
+<form action="" method="POST">
+	Komentar : <input type="text" name="komentar">
+	<input type="submit" name="submit" value="submit">
+	<?php if (isset(var)) {
+		echo $err;
+	} ?>
+</form>
 
+<?php
+	if (isset($_POST['submit'])) {
+		$komentar = $_POST['komentar'];
+		$err ="";
+
+		$jmlKata = str_word_count($komentar);
+		if ($jmlKata < 5) {
+			$err = "ERROR";
+		}
+
+	}
 ?>
